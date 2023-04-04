13d35c6b76b386c302640e6e100cfbfcf412e44e
frankmagoba <franklinmagoba4@gmail.com>
2023-04-04 19:12:49 +0300

    Updates

---
+++ precache-manifest.57e9fc0c9e8af4a4dc40947fbba60173.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4620f4ac049629d9f3d22ed4367eb422",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "d94df6c3e801fcdfee5a",
+    "url": "/Resume/static/css/main.241ec352.chunk.css"
+  },
+  {
+    "revision": "c5b74f03c01b9e4aaf83",
+    "url": "/Resume/static/js/2.5f88743e.chunk.js"
+  },
+  {
+    "revision": "d94df6c3e801fcdfee5a",
+    "url": "/Resume/static/js/main.8ec1efcb.chunk.js"
+  },
+  {
+    "revision": "8aafb980d57a0763be84",
+    "url": "/Resume/static/js/runtime-main.92b4f6db.js"
+  },
+  {
+    "revision": "9e7e9e73da6fdf21ed1b33ec32c2ea36",
+    "url": "/Resume/static/media/Azure_ ActiveDirectory.9e7e9e73.png"
+  },
+  {
+    "revision": "451624d5b61625837f1f7ed67c98ae21",
+    "url": "/Resume/static/media/MicrosoftCloudSolution.451624d5.png"
+  },
+  {
+    "revision": "18a44f504844e38b9c05e5fbd188d231",
+    "url": "/Resume/static/media/OpenCV.18a44f50.png"
+  },
+  {
+    "revision": "2b795c243d20922f12e3b8f569b94059",
+    "url": "/Resume/static/media/advancedcloudconcept.2b795c24.png"
+  },
+  {
+    "revision": "e627b981efd22eaf4a4f4c77c6f5fb41",
+    "url": "/Resume/static/media/agile.e627b981.png"
+  },
+  {
+    "revision": "cca8e49313431c016cabbcd114e3f283",
+    "url": "/Resume/static/media/alc_badge_hours_franklinMagoba.cca8e493.png"
+  },
+  {
+    "revision": "a8f87db59ef00aa94a739f79b2e15f31",
+    "url": "/Resume/static/media/angular.a8f87db5.png"
+  },
+  {
+    "revision": "282445e0b241da06bb23ad77465d09c2",
+    "url": "/Resume/static/media/az900exam.282445e0.png"
+  },
+  {
+    "revision": "fbe269ed1d0eb07263812d35185dc42e",
+    "url": "/Resume/static/media/azuese1.fbe269ed.png"
+  },
+  {
+    "revision": "ea75047639217ed3355f0fd55749b669",
+    "url": "/Resume/static/media/azuese2.ea750476.png"
+  },
+  {
+    "revision": "31bf3c6b9eca532b265dba6a045aaead",
+    "url": "/Resume/static/media/azureactivedir.31bf3c6b.png"
+  },
+  {
+    "revision": "fc81fa70d5dd7ef92a7f7f421d6ac0b1",
+    "url": "/Resume/static/media/azuretools.fc81fa70.png"
+  },
+  {
+    "revision": "5cf36637aa94d0ba9100895e445f206c",
+    "url": "/Resume/static/media/cloudcore.5cf36637.png"
+  },
+  {
+    "revision": "e8a05469d0a279c52bc8105b67861827",
+    "url": "/Resume/static/media/cloudintro.e8a05469.png"
+  },
+  {
+    "revision": "bf0f97b3f0f128e662fb97ac8810ef56",
+    "url": "/Resume/static/media/coreCert.bf0f97b3.png"
+  },
+  {
+    "revision": "c941aeb05689e536f7668366d8ef1dbe",
+    "url": "/Resume/static/media/databases.c941aeb0.png"
+  },
+  {
+    "revision": "6ea1500b6614312b0a31a1e8710d898b",
+    "url": "/Resume/static/media/datascience.6ea1500b.png"
+  },
+  {
+    "revision": "69fa5c9a82a16e6f15bbc1707e0bd1e9",
+    "url": "/Resume/static/media/fullstack.69fa5c9a.png"
+  },
+  {
+    "revision": "73ef6d97d3c89381bcfda20973f2ea62",
+    "url": "/Resume/static/media/net.73ef6d97.png"
+  },
+  {
+    "revision": "91940cb2a227494143cc9f42a3987b95",
+    "url": "/Resume/static/media/node.91940cb2.png"
+  },
+  {
+    "revision": "fb755095e3f0b6b18492e41e58f575b8",
+    "url": "/Resume/static/media/react.fb755095.png"
+  },
+  {
+    "revision": "7bfaafd6e567169f8b522578fe931430",
+    "url": "/Resume/static/media/restapi.7bfaafd6.png"
+  },
+  {
+    "revision": "715bcfd8b064b9cc68183e0559e51154",
+    "url": "/Resume/static/media/security.715bcfd8.png"
+  },
+  {
+    "revision": "e850d08955e49c13e60107d40ee2da13",
+    "url": "/Resume/static/media/storage.e850d089.png"
+  },
+  {
+    "revision": "c0bd4a260ba00f64d79ef8c2ed74ddfc",
+    "url": "/Resume/static/media/subscriptions.c0bd4a26.png"
+  }
+]);
